546432f2bcbd95a7f084bbd57a33ddfed0b5a08e
napling <matthewcellis98@gmail.com>
2024-10-26 16:28:39 -0500

    named many to many rrelationships

--- main/models/index.js
+++ main/models/index.js
@@ -19,7 +19,8 @@ Product.belongsToMany(Tag, {
     through: {
         model: ProductTag,
         unique: false
-    }
+    },
+    as: 'product_tags'
 })
 
 // Tags belongToMany Products (through ProductTag)
@@ -27,7 +28,8 @@ Tag.belongsToMany(Product, {
     through: {
         model: ProductTag,
         unique: false
-    }
+    },
+    as: 'tagged_products'
 })
 
 module.exports = {
