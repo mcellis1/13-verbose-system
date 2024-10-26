22a3f00daf5e103497379c5f3190ed54e4f93e0e
napling <matthewcellis98@gmail.com>
2024-10-26 16:28:58 -0500

    added returns

--- main/routes/api/product-routes.js
+++ main/routes/api/product-routes.js
@@ -133,6 +133,7 @@ router.delete('/:id', async (req, res) => {
         })
         if (!productData) {
             res.status(404).json({ message: 'no product with this id found' })
+            return
         }
         res.status(200).json(productData)
     } catch (err) {
