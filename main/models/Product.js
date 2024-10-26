883e8a26e6aea609d69ebdc78e9e1d6367b53357
napling <matthewcellis98@gmail.com>
2024-10-26 13:24:21 -0500

    defined product columns

--- main/models/Product.js
+++ main/models/Product.js
@@ -10,6 +10,32 @@ class Product extends Model { }
 Product.init(
     {
         // define columns
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        location_name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        price: {
+            type: DataTypes.DECIMAL(10, 2),
+            allowNull: false
+        },
+        stock: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        category_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'category',
+                key: 'id',
+                unique: false
+            }
+        }
     },
     {
         sequelize,
