84ce923a47550379d916c4f5a25bd01313095da2
napling <matthewcellis98@gmail.com>
2024-10-26 13:24:32 -0500

    defined product tag columns

--- main/models/ProductTag.js
+++ main/models/ProductTag.js
@@ -7,6 +7,22 @@ class ProductTag extends Model { }
 ProductTag.init(
     {
         // define columns
+        product_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'product',
+                key: 'id',
+                unique: false
+            }
+        },
+        tag_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'tag',
+                key: 'id',
+                unique: false
+            }
+        }
     },
     {
         sequelize,
