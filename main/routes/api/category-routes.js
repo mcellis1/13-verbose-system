68f519cbc1e219da04c80fb5519744154961dbbd
napling <matthewcellis98@gmail.com>
2024-10-26 20:30:11 -0500

    corrected typo in the category routes

--- main/routes/api/category-routes.js
+++ main/routes/api/category-routes.js
@@ -36,7 +36,7 @@ router.get('/:id', async (req, res) => {
 router.post('/', async (req, res) => {
     // create a new category
     try {
-        const categoryData = await Location.create(req.body)
+        const categoryData = await Category.create(req.body)
         res.status(200).json(categoryData);
     } catch (err) {
         res.status(400).json(err)
